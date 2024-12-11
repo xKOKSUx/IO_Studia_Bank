42a55d4b78c9a36c025295eff833462558a60784
Lucifer <SurdackiD1@gmail.com>
2024-12-11 17:01:42 +0100

    Added an option to add money to an account

---
+++ Bank/src/Entities/AddValue.cs
@@ -0,0 +1,16 @@
+﻿namespace Bank.src.Entities
+{
+    public class AddValue
+    {
+        public int Value { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+        public AddValue(int value, string description, DateTime date)
+        {
+            Value = value;
+            Description = description;
+            Date = date;
+        }
+    }
+}
